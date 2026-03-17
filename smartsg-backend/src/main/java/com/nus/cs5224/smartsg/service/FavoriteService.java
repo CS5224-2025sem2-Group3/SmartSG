b6d7de872a34ad8931bff033f17c4491fcc352ab
cp152 <cp15251276582@outlook.com>
2026-03-17 21:51:44 +0800

    Revert "complete favorite api"
    This reverts commit 62c92ef069ded38f4e15e7d69fdbe6056ae83bfc.

--- smartsg-backend/src/main/java/com/nus/cs5224/smartsg/service/FavoriteService.java
+++ smartsg-backend/src/main/java/com/nus/cs5224/smartsg/service/FavoriteService.java
@@ -1,11 +1,4 @@
 package com.nus.cs5224.smartsg.service;
 
-import com.nus.cs5224.smartsg.dto.response.FavoriteResponse;
-
-import java.util.List;
-
 public interface FavoriteService {
-    public List<FavoriteResponse> getFavorites(Long userId);
-    public void addFavorite(Long userId, Long listingId);
-    public void removeFavorite(Long userId, Long listingId);
 }
